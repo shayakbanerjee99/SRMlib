42bb3595146b2968bdcc91867f171326b3a3c8e8
Shayak Banerjee <shayak.banerjee.13@gmail.com>
2019-02-07 01:18:08 +0530

    Refactored the code and added comments
    Changed 'Login in' to 'Login' in SignUp page
    
    Changed 'text_orsignin' to 'text_login'

--- app/src/main/java/aakashresearchlab/com/srmlib/SignUp.java
+++ app/src/main/java/aakashresearchlab/com/srmlib/SignUp.java
@@ -14,21 +14,17 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
 
 /**
  * Created by harshit on 02-12-2017.
+ *
+ * @author Harshit Gupta
+ * @since 2nd Dec 2017
  */
 
 public class SignUp extends AppCompatActivity implements View.OnClickListener {
@@ -36,14 +32,15 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener {
     private FirebaseAuth.AuthStateListener mAuthListener;
     private CardView button_sign_up;
     private ProgressDialog pdLoading;
-    private TextView text_username, text_password, text_orsignin, text_forgot_password;
+    private TextView text_username, text_password, text_login, text_forgot_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
-        mAuth = FirebaseAuth.getInstance();
 
+        // Connect to Firebase
+        mAuth = FirebaseAuth.getInstance();
         mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
@@ -55,38 +52,49 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener {
                 }
             }
         };
+
+        // Prevent user from interacting with the application while processing data
         pdLoading = new ProgressDialog(this);
         pdLoading.setMessage("Signing In...");
-        button_sign_up = (CardView) findViewById(R.id.bsignup);
-        text_password = (TextView) findViewById(R.id.sfield_password);
-        text_username = (TextView) findViewById(R.id.sfield_username);
-        text_orsignin = (TextView) findViewById(R.id.orsignin);
-        text_forgot_password = (TextView) findViewById(R.id.sfield_forgpass);
+
+        button_sign_up = findViewById(R.id.bsignup); // card view
+        text_password = findViewById(R.id.sfield_password);
+        text_username = findViewById(R.id.sfield_username);
+        text_login = findViewById(R.id.text_login);
+        text_forgot_password = findViewById(R.id.sfield_forgpass);
 
         button_sign_up.setOnClickListener(this);
         text_forgot_password.setOnClickListener(this);
-        text_orsignin.setOnClickListener(this);
+        text_login.setOnClickListener(this);
     }
 
-    private void Signup() {
+    // the below function will be called when button_sign_up will be clicked
+    private void signUp() {
         pdLoading.show();
         final String email, password;
         email = text_username.getText().toString().trim();
         password = text_password.getText().toString().trim();
+
+        // if the user leaves an empty field
         if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
 
             Toast.makeText(SignUp.this, "Empty Fields", Toast.LENGTH_SHORT).show();
             pdLoading.dismiss();
-        } else {
+
+        } else { // good input
+
+            // create a new user in the database
             mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                 @Override
                 public void onComplete(@NonNull Task<AuthResult> task) {
-                    if (task.isSuccessful()) {
+                    if (task.isSuccessful()) { // new account is successfully created
                         Toast.makeText(SignUp.this, "Account Updated", Toast.LENGTH_SHORT).show();
                         pdLoading.dismiss();
                         finish();
-                        Intent i = new Intent(SignUp.this, MainActivity.class);
-                        startActivity(i);
+
+                        // launch MainActivity once the new user account has been created
+                        Intent intent = new Intent(SignUp.this, MainActivity.class);
+                        startActivity(intent);
                     } else {
                         pdLoading.dismiss();
                         Toast.makeText(SignUp.this, "An error occurred..", Toast.LENGTH_SHORT).show();
@@ -112,18 +120,18 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     public void onClick(View v) {
-        if (v == button_sign_up) {
-            Signup();
-        } else if (v == text_forgot_password) {
+        if (v == button_sign_up) { // if button_sign_up is clicked
+            signUp();
+        } else if (v == text_forgot_password) { // if text_forgot_password is clicked
             AlertDialog.Builder builder = new AlertDialog.Builder(this);
             builder.setTitle("Enter your email");
-            final EditText editmail = new EditText(this);
-            builder.setView(editmail);
+            final EditText edit_text_mailId = new EditText(this);
+            builder.setView(edit_text_mailId);
             builder.setPositiveButton("Send Mail", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
-                    String mailid = editmail.getText().toString().trim();
-                    mAuth.sendPasswordResetEmail(mailid).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    String mailId = edit_text_mailId.getText().toString().trim();
+                    mAuth.sendPasswordResetEmail(mailId).addOnCompleteListener(new OnCompleteListener<Void>() {
                         @Override
                         public void onComplete(@NonNull Task<Void> task) {
                             if (task.isSuccessful()) {
@@ -136,9 +144,9 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener {
                 }
             });
             builder.show();
-        } else if (v == text_orsignin) {
-
-            Intent i = new Intent(SignUp.this,MainActivity.class);
+        } else if (v == text_login) { // if the user wishes to login instead
+            // Launch MainActivity
+            Intent i = new Intent(SignUp.this, MainActivity.class);
             finish();
             startActivity(i);
         }
